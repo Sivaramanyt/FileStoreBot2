838ebd0a3dbe8782c43d64686bbd0308edbe1af0
sivaraman444 <119070317+Sivaramanyt@users.noreply.github.com>
2025-08-28 10:50:20 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,16 +1,16 @@
 import os, json
 
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8219734500:AAG3hD8fPirprlr4OZEYF4oLr09UVWr_e9s")
 BASE_URL = os.getenv("BASE_URL", "")  # e.g. https://yourapp-org.koyeb.app
 WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET", "")
 
-MONGO_URL = os.getenv("MONGO_URL", "")
-DB_NAME = os.getenv("DB_NAME", "filestorebot")
+MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://Sivaraman444:Rama9789@cluster0.8lxln.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = os.getenv("DB_NAME", "cluster0")
 
-OWNER_ID = int(os.getenv("OWNER_ID", "0") or 0)
+OWNER_ID = int(os.getenv("OWNER_ID", "1206988513") or 0)
 ADMINS = [int(x) for x in os.getenv("ADMINS", "").split() if x.strip().isdigit()]
 
-FORCE_SUB_CHANNELS = [int(x) for x in os.getenv("FORCE_SUB_CHANNELS", "").split(",") if x.strip().startswith("-100")]
+FORCE_SUB_CHANNELS = [int(x) for x in os.getenv("FORCE_SUB_CHANNELS", "-1002429072244").split(",") if x.strip().startswith("-100")]
 FREE_VIDEO_LIMIT = int(os.getenv("FREE_VIDEO_LIMIT", "3"))
 
 SHORTLINK_PROVIDERS = json.loads(os.getenv("SHORTLINK_PROVIDERS", "[]"))
